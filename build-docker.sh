0f0ea9b8d75fd325a1d874df876452b0ae28e719
Pablo Werlang <pswerlang@gmail.com>
2024-10-31 19:40:37 -0300

    add build script.sh

---
+++ build-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <tag>"
+    exit 1
+fi
+
+TAG=$1
+
+docker buildx build --platform linux/amd64,linux/arm64 -t pswerlang/nginx-auto-ssl:$TAG --push .
